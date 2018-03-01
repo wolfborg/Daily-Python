259e4da68ad0384b53fc05b71636d970d19d8c94
Derek Chaplin <wolfborg@users.noreply.github.com>
2018-03-01 13:57:16 -0500

    Some kata exercises

---
+++ 2018-03-01/kata.py
@@ -0,0 +1,11 @@
+def nearest_sq(n):
+	return round(n**0.5)**2
+
+def number(bus_stops):
+	return sum([stop[0]-stop[1] for stop in bus_stops])
+
+def toCsvText(array):
+	return '\n'.join(','.join(map(str,line)) for line in array)
+
+def maps(a):
+	return [x*2 for x in a]
